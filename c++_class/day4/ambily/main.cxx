ced1987e151be9a4b146e11edeeee83f6c7469f1
ambilyganga <ambilyganga1612@gmail.com>
2024-10-22 22:58:51 +0530

    Update main.cxx

--- c++_class/day4/ambily/main.cxx
+++ c++_class/day4/ambily/main.cxx
@@ -1,1 +1,19 @@
 #include<iostream>
+
+#define pi 22/7
+static const float e0 = 1;
+float force (float(q1), float(q2), float(r1),)
+
+void main ()
+{
+    float r1 = 1, r2 = 2;
+    float  q1 = 1 , q2 = 1;
+    float q = 1;
+    float m = 1 ;
+    float r , F12 ;
+    float r = r2 - r1 ;
+} 
+
+
+
+
