4052271b366b4c51a7adf7b073020eae2319a722
ambilyganga <ambilyganga1612@gmail.com>
2024-10-23 15:07:26 +0530

    Update main.cxx

--- c++_class/day4/ambily/main.cxx
+++ c++_class/day4/ambily/main.cxx
@@ -1,17 +1,36 @@
 #include<iostream>
 
-#define pi 22/7
-static const float e0 = 1;
-float force (float(q1), float(q2), float(r1),)
+#define pi 22/7 // for defining no semicolon and equal is needed
+static const float e0 = 1; // to store in RAM
 
-void main ()
+float force (float q1, float q2, float r1, float r2)
+
+{
+    return 1 / ( 4 * pi * e0 ) * q1 * q2 / ( r2 - r1) * ( r2- r1);
+}
+
+
+int main ()
 {
     float r1 = 1, r2 = 2;
     float  q1 = 1 , q2 = 1;
     float q = 1;
     float m = 1 ;
-    float r , F12 ;
-    float r = r2 - r1 ;
+    float dt = 0.001;
+    float f = force(1,1,1,2);
+
+    float a = f / m ; // accelaration
+
+    float v = 0; //initial condition
+
+    v = v + a * dt; // velocity
+
+    float s = v * dt;
+
+    std :: 
+
+
+
 } 
 
 
