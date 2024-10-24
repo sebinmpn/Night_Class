42cb7fcc5de2c3ce4099b0925a32ce997cef2c3d
sebinmpn <sebinfrancis1999@gmail.com>
2024-10-24 22:25:55 +0530

    cleanning the code form heaven

--- c++_class/day4/anu/main.cxx
+++ c++_class/day4/anu/main.cxx
@@ -1,18 +1,22 @@
 #include<iostream>
-  #define pi 22/7
-  static const float e0=1;
-  float force (float q1,float q2,float r1,float r2);
-  void main()
-    {
-float q1=1;
-float q2=1;
-float e0=1;
-float r1=1;
-float r2=2;
-float F12;
-float m=1;
-r= r2-r1;
-F12 = 1/(4*pi *e0) q1*q2/(r^2)
+  
+#define pi 22/7
+  
+static const float e0 = 1;
+  
+float force (float q1, float q2, float r1, float r2);
+  
+int main()
+{
+  float q1 = 1;
+  float q2 = 1;
+  float e0 = 1;
+  float r1 = 1;
+  float r2 = 2;
+  float F12;
+  float m = 1;
+  float r21 = r2-r1;
 
-  }
+  F12 = 1 / (4 * pi * e0) * q1 * q2 / (r21 * r21);
+}
 
